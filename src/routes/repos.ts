efd7819de65f8580805049718977cd954d528f42
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-05-18 14:21:30 -0400

    added endpoint for fetching and filtering repos data

--- src/routes/repos.ts
+++ src/routes/repos.ts
@@ -1,6 +1,15 @@
 import { Router, Request, Response } from 'express';
+import axios from 'axios';
+import { Repo } from '../models/Repo';
+import jsonData from '../../data/repos.json';
+import dotenv = require('dotenv');
+
+dotenv.config();
 
 export const repos = Router();
+const URL = process.env.URL as string;
+
+const isForkedFalse = (item: Repo) => item.fork === false;
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
@@ -8,5 +17,15 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  const reposData = await axios.get(URL);
+
+  const filteredUrlData: Repo[] = reposData.data.filter((item: Repo) =>
+    isForkedFalse(item)
+  );
+  const filteredJsonData: Repo[] = jsonData.filter((item) =>
+    isForkedFalse(item)
+  );
+  const filteredData = filteredUrlData.concat(filteredJsonData);
+
+  res.json(filteredData);
 });
